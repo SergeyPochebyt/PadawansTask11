5bafd22221f15d197c1d454f63d0ba3fda016626
unknown <sergeymailmobile@gmail.com>
2019-02-22 18:33:56 +0300

    Test 1

---
+++ ArrayExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PadawansTask11
+{
+    public static class ArrayExtensions
+    {
+        public static int? FindIndex(double[] array, double accuracy)
+        {
+
+			if (array == null) throw new ArgumentNullException();
+            if (accuracy <= 0 || accuracy >= 1) throw new ArgumentException();
+            
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                double sum1 = 0;
+                double sum2 = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    sum1 = sum1 + array[j];
+                }
+
+                for (int j = array.Length - 1; j > i; j--)
+                {
+                    sum2 = sum2 + array[j];
+                }
+
+                if (Math.Abs(sum1 - sum2) < accuracy)
+                {
+                    return i;
+                }
+            }
+            return null;
+
+        }
+    }
+}
